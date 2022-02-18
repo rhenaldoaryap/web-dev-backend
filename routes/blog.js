189b54ae6ff2249861f59e814d21dd5d5421b0ae
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-02-18 18:01:49 +0700

    fetching and displaying single blog post

--- routes/blog.js
+++ routes/blog.js
@@ -32,13 +32,29 @@ router.post("/posts", async function (req, res) {
     req.body.author,
   ];
   // (?) means placeholder that will hold the data from the body and will output and array
-  // [data] at the second parameter will hold all of that data
+  // [data] at the second parameter will hold all of that data and will fill in the (?) placeholder
   await db.query(
     "INSERT INTO posts (title, summary, body, author_id) VALUES (?)",
     [data]
   );
 
   res.redirect("/posts");
+});
+
+router.get("/posts/:id", async function (req, res) {
+  const query = `
+    SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts
+    INNER JOIN authors ON posts.author_id = authors.id
+    WHERE posts.id = ?
+  `;
+
+  const [posts] = await db.query(query, [req.params.id]);
+
+  if (!posts || posts.length === 0) {
+    return res.status(404).render("404");
+  }
+
+  res.render("post-detail", { post: posts[0] });
 });
 
 module.exports = router;
